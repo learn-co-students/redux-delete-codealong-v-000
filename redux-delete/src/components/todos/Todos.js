47239d0a7975008c18644a1089018ebfa1c21be5
github username <github email address>
2017-03-06 18:29:51 -0500

    Done.

--- redux-delete/src/components/todos/Todos.js
+++ redux-delete/src/components/todos/Todos.js
@@ -3,9 +3,10 @@ import Todo from './Todo'
 
 class Todos extends Component {
   render(){
-    let todos = this.props.store.getState().todos.map((todo) => {
-      return <Todo text={todo.text} id={todo.id} store={this.props.store} />
-    })
+    const todos = this.props.store.getState().todos.map((todo, index) => {
+      return <Todo text={todo.text} key={index} id={todo.id} /* <- code change */ store={this.props.store} />
+    });
+
     return(
       <ul>
         {todos}
