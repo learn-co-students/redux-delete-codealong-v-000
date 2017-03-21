79409e92adeb9a4dd9c63655e7a2b6eb9516cf9e
cmiller36 <m.calliemiller@gmail.com>
2017-03-21 17:32:14 -0400

    Done.

--- redux-delete/src/reducers/manageTodo.js
+++ redux-delete/src/reducers/manageTodo.js
@@ -1,11 +1,17 @@
-let id = 0
-export default function manageTodo(state = {todos: []}, action){
+let id = 0;
+ 
+export default function manageTodo(state = {
+  todos: []
+}, action) {
   switch (action.type) {
     case 'ADD_TODO':
-    id++
-    let todo = Object.assign({}, action.payload, {id: id})
-      return {todos: state.todos.concat(todo)}
+      id++;
+      const todo = Object.assign({}, action.todo, { id: id });
+      return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      const todos = state.todos.filter(todo => todo.id !== action.id);
+      return  { todos }
     default:
       return state;
   }
-}
+};
