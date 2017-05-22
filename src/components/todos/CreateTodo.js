9ff166768f5dcc4fda9b9312a06d3e6f12b11752
Brad Watson <brad.watson.orlando@gmail.com>
2017-05-22 10:44:27 -0400

    Done

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -10,7 +10,7 @@ class CreateTodo extends Component {
   }
   handleSubmit(event){
     event.preventDefault()
-    this.props.store.dispatch({type: 'ADD_TODO', payload: this.state})
+    this.props.store.dispatch({type: 'ADD_TODO', todo: this.state})
     this.setState({text: ''})
   }
   render(){
