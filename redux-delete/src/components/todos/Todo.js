25e4cdd0839702f5af10fe9334ee62b44ddf0c8b
liahwallace1 <liahwallace1@gmail.com>
2017-06-07 10:31:44 -0400

    Done.

--- redux-delete/src/components/todos/Todo.js
+++ redux-delete/src/components/todos/Todo.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 class Todo extends Component {
+  handleDelete() {
+    this.props.store.dispatch({
+      type: 'DELETE_TODO',
+      id: this.props.id,
+    })
+  }
+
   render(){
     return (
       <li>
         {this.props.text}
+        <button onClick={() => this.handleDelete()} />
       </li>
     )
   }
