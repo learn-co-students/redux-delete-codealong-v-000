0cba5ec5058ab3a6851bc7901be75ac2c7056a95
MargaritaKhodzka <mkhodzka@gmail.com>
2017-11-20 16:09:08 -0500

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -4,9 +4,12 @@ import Todo from './Todo'
 class Todos extends Component {
 
   render() {
-
+/*
+pass the delete action through from the parent component
+to have the ability to dispatch an action to the store from each Todo
+*/
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
+      return <Todo text={todo.text} key={index} id={todo.id} store={this.props.store} />
     });
 
     return(
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,10 +4,17 @@ export default function manageTodo(state = {
   todos: []
 }, action) {
   switch (action.type) {
+    //creates the Todo when a CREATE_TODO action is dispatched
+    //implement a counter
     case 'ADD_TODO':
       id++;
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+
+    case 'DELETE_TODO':
+      const todos = state.todos.filter(todo => todo.id !== action.id)
+      return {todos}
+
     default:
       return state;
   }
