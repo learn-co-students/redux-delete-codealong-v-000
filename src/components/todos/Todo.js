2ffea0b3969653541215bca1d6de30ebc34b32d3
Sophia Hubeny <shhubeny@gmail.com>
2017-12-22 17:09:09 +0000

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_TODO',
+      id: this.props.id, //each component has the correct id as the prop so this just needs to be sent along
+    })
+  }
+
   render() {
     return (
       <li>
         {this.props.text}
+        <button onClick={this.handleOnClick}/> //clicked, calls back the function, dispatches action
       </li>
     );
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -5,9 +5,12 @@ export default function manageTodo(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
-      id++;
+      id++; //counter, variable is local to this file
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      const todos = state.todos.filter(todo => todo.id !== action.id);
+      return {todos}
     default:
       return state;
   }
