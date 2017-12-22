26b6477bb21fff70dbb0ba04d6be5678022e1a01
Sophia Hubeny <shhubeny@gmail.com>
2017-12-22 17:09:30 +0000

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -9,7 +9,7 @@ export default function manageTodo(state = {
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
     case 'DELETE_TODO':
-      const todos = state.todos.filter(todo => todo.id !== action.id);
+      const todos = state.todos.filter(todo => todo.id !== action.id); //filter method returns all the existing todos except the one wth the id that needs to be deleted
       return {todos}
     default:
       return state;
