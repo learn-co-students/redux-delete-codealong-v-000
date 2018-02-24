c2474fbf5a8aaec00fcb7b41f338dfe5973cb6c1
Soumya Veer <veer.soumya@gmail.com>
2018-02-24 10:42:58 -0800

    complete delete codealong

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 import Todo from './Todo'
 
 class Todos extends Component {
-
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
+      return <Todo text={todo.text} key={index} id={todo.id} store={this.props.store} />
     });
 
     return(
