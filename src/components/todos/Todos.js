f3243106abb5138a22ce6e661b63d953aa0bc416
github username <github email address>
2018-03-21 10:27:27 -0600

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import Todo from './Todo'
+import React, { Component } from 'react';
+import Todo from './Todo';
 
 class Todos extends Component {
 
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
+      return <Todo text={todo.text} key={index} id={todo.id} /* <- code change */ store={this.props.store} />
     });
 
-    return(
+    return (
       <ul>
         {todos}
       </ul>
