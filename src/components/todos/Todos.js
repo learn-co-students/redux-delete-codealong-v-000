cbc12831b258e3e55c68491894bc6b6b558ae89a
Christian Egglin <cegglin@conncoll.edu>
2018-04-07 23:07:15 +0000

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react'
 import Todo from './Todo'
 
 class Todos extends Component {
-
   render() {
-
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
+      return <Todo key={index} text={todo.text} id={todo.id} store={this.props.store} />
     });
 
     return(
