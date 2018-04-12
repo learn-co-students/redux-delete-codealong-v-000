3f3e084eb3f44f1c543ce9bafc6353201af3db3c
Autumn Kangas <autumnkangas@gmail.com>
2018-04-12 16:05:13 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,13 +1,18 @@
 let id = 0;
+//Because we are using modules, this variable, while not enclosed in a function, is local to the file
 
 export default function manageTodo(state = {
   todos: []
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
+      //to find and delete items later, we need to identify them by ID
       id++;
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      const todos = state.todos.filter(todo => todo.id !== action.id);
+      return  { todos }
     default:
       return state;
   }
