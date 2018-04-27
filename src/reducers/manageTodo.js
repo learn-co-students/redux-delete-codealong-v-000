f68372e1f76b2dc4c5af1004f10c459650e7bdca
Igor Eskin <github email address>
2018-04-27 16:43:42 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -6,8 +6,12 @@ export default function manageTodo(state = {
   switch (action.type) {
     case 'ADD_TODO':
       id++;
-      const todo = Object.assign({}, action.todo, { id: id });
+      const todo = {...action.todo, id: id}
+      // const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      return {todos: state.todos.filter(todo => todo.id !== action.id)}
+
     default:
       return state;
   }
