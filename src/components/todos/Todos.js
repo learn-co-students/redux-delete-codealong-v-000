13d7749d8dc82b9ee535c3805d942c4fcf44c2aa
brittmmendez <brittmmendez@gmail.com>
2018-05-12 02:07:12 -0400

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -6,7 +6,8 @@ class Todos extends Component {
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
+      //we don't get access to the store automatically, we have to pass it through from the parent component - also pass in id to use in deleting process
+      return <Todo text={todo.text} key={index} id={todo.id} store={this.props.store}/>
     });
 
     return(
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,3 +1,4 @@
+//implement a counter
 let id = 0;
 
 export default function manageTodo(state = {
@@ -8,6 +9,10 @@ export default function manageTodo(state = {
       id++;
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      //when the DELETE_TODO action is sent, we use the filter method to return all of the existing todos except the one with the id sent in the action. The filter method returns to us a new array, which we then return.  
+      const todos = state.todos.filter(todo => todo.id !== action.id);
+      return  { todos }
     default:
       return state;
   }
