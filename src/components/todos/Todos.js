7ed09eb52b20f2406dd834af6ff207402adedbf8
github username <schun08@gmail.com>
2018-05-22 21:16:15 -0400

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import Todo from './Todo'
 
+// need to make sure to pass through the text and id to each Todo 
+
 class Todos extends Component {
 
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
+      return <Todo text={todo.text} key={index} id={todo.id} store={this.props.store} />
     });
 
-    return(
+    return (
       <ul>
         {todos}
       </ul>
