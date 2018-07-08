28b9d217925cf092b385b7218fecc26ba4227d9b
github username <github email address>
2018-07-08 11:18:38 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  handleDeleteClick() {
+    this.props.store.dispatch(
+      {
+        type: 'DELETE_TODO',
+        id: this.props.id,
+      }
+    )
+  }
+
   render() {
     return (
       <li>
         {this.props.text}
+        <button onClick={this.handleDeleteClick.bind(this)} >Delete</button>
       </li>
     );
   }
