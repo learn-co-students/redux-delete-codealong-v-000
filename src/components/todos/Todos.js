87f104b13e1c6f5b20a43da54beb3f6e019ca40a
Canino Developments <ian.canino@gmail.com>
2018-07-24 00:01:21 -0400

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react'
 import Todo from './Todo'
-
+ 
 class Todos extends Component {
-
-  render() {
-
+  render () {
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} />
-    });
-
-    return(
+      return <Todo text={todo.text} key={index} id={todo.id} /* <- code change */ store={this.props.store} />
+    })
+ 
+    return (
       <ul>
         {todos}
       </ul>
-    );
+    )
   }
 };
 
-export default Todos;
+export default Todos
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,14 +1,18 @@
-let id = 0;
-
-export default function manageTodo(state = {
+let id = 0
+ 
+export default function manageTodo (state = {
   todos: []
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
-      id++;
-      const todo = Object.assign({}, action.todo, { id: id });
-      return { todos: state.todos.concat(todo) };
+      id++
+      const todo = Object.assign({}, action.todo, { id: id })
+      return { todos: state.todos.concat(todo) }
+    case 'DELETE_TODO':
+      const todos = state.todos.filter(todo => todo.id !== action.id)
+      return { todos }
     default:
-      return state;
+      return state
   }
 };
+
