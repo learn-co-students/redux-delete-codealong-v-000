3d7eac5cc0159171a0279e748dea514d6c332de2
mrfarmer7771 <mfarmer@d120.org>
2018-08-15 01:07:16 +0000

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,7 +4,9 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  //instead of using the index as the id (and HTML id), we use the freshly-implemented 15-digit number
+  //Which we can use to surgically delete the to-do later,we also pass the whole object down as a prop (to-do)
+  renderTodos = () => this.props.todos.map((todo) => <Todo delete={this.props.delete} key={todo.id} todo={todo} />)
 
   render() {
     return(
@@ -15,10 +17,16 @@ class TodosContainer extends Component {
   }
 };
 
-mapStateToProps = state => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText=> dispatch({type: "DELETE_TODO", payload: todoText})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
