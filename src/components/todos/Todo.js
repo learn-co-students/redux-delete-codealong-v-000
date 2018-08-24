0ddbc2c0f11d7063798992cb718334f7dfbad7d7
Raiet <raietsaeed@gmail.com>
2018-08-24 20:17:29 +0000

    add/delete todos using todo text

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+const Todo = props => {
+  return (
+    <div>
+    
+        {/* Note : Adding a click listener but using an anonymous function (rather than a separate named one) to keep this component small */}
+        <span>{props.text}</span><button onClick={() => props.delete(props.text)}>DELETE</button>
+    </div>
+  )
+}
 
 export default Todo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,10 +1,16 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
+  console.log(action);
 
+  switch (action.type) {
+    
+    case 'ADD_TODO':
       return { todos: state.todos.concat(action.payload.text) };
+
+    // Note : Filtering is a brief way of displaying todos that don't match the specified one (using text content)
+    case 'DELETE_TODO':
+      return {todos: state.todos.filter(todo => todo !== action.payload)}
 
     default:
       return state;
