47caacb57f1335d4e77e8cc266e674e732904ec6
Cristina Lugo <cmlugoce@syr.edu>
2018-09-11 14:58:12 +0000

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,11 +1,20 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  console.log(action);
   switch (action.type) {
     case 'ADD_TODO':
 
-      return { todos: state.todos.concat(action.payload.text) };
 
+          const todo = {
+            id: Math.random()*10000000000000000,
+            text: action.text
+          }
+          return {...state, bands: [...state.bands, todo]};
+
+        case 'DELETE_TODO':
+
+          return {todos: state.todos.filter(todo => todo !== action.id)}
     default:
       return state;
   }
