0ca3a1ba84cf392ef45465ffb03db1c761f93c64
Joanne Lee <vernapii@gmail.com>
2018-09-13 11:05:25 -0700

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,9 +2,15 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
   switch (action.type) {
+    console.log(action);
+  switch (action.type) {
     case 'ADD_TODO':
 
       return { todos: state.todos.concat(action.payload.text) };
+
+    case 'DELETE_TODO':
+
+    return {todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
