8448462245038013e5324acb23e0b9ec1e62744c
BrownCow371 <jmbrowncow@gmail.com>
2018-10-25 09:24:52 -0700

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,8 +3,15 @@ export default function manageTodo(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_TODO':
+    const todo = {
+      id: Math.random()*1000000,
+      text: action.payload.text
+    }
+      return { todos: state.todos.concat(todo) };
 
-      return { todos: state.todos.concat(action.payload.text) };
+    case 'DELETE_TODO':
+        console.log("action", action);
+        return {todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
