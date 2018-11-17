d46bde201848e493b8b2e02e66df5d3fb4b3ebc9
Deborah Seow <deborahseow9@gmail.com>
2018-11-16 18:40:20 -0600

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+// const Todo = props => <li>{props.text}</li>
+
+const Todo = props => {
+  return (
+    <div>
+      <span>{props.todo.text}</span>
+      <button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+      {/*Button doesn't need a value attribute - what is inside it is the value*/}
+    </div>
+  )
+}
 
 export default Todo;
