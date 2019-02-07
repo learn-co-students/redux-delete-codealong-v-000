5df0dabe13ba507038a48f75a8a9193ef5fdd55d
Micah Shute <micah.shute@gmail.com>
2019-02-06 22:58:51 -0600

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+const Todo = props => {
 
+    return(
+        <li>
+            <span>{props.todo.text}</span>
+            <button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+        </li>
+    )
+
+
+}
 export default Todo;
