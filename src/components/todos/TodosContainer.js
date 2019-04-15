747dd6395be2404da3769a71385fe2e6f9f343aa
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-04-14 20:45:46 -0600

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -15,10 +15,18 @@ class TodosContainer extends Component {
   }
 };
 
-mapStateToProps = state => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({
+      type: 'DELETE_TODO',
+      payload: todoText })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
