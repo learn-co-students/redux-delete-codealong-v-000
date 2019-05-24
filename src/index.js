ee7851c352431daa654d8afda7afeae3c172539f
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-05-24 16:58:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import manageTodo from './reducers/manageTodo';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import manageTodo from "./reducers/manageTodo";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-let store = createStore(manageTodo);
+let store = createStore(
+  manageTodo,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,10 +1,24 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
+import uuid from "uuid";
 
-      return { todos: state.todos.concat(action.payload.text) };
+export default function manageTodo(
+  state = {
+    todos: []
+  },
+  action
+) {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TODO":
+      const todo = {
+        id: uuid(),
+        text: action.payload.text
+      };
+      return { todos: state.todos.concat(todo) };
+
+    case "DELETE_TODO":
+      return {
+        todos: state.todos.filter(todo => todo.id !== action.payload)
+      };
 
     default:
       return state;
