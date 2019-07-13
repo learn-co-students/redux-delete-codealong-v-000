e5461a3edda32d3a5da4cdc23ae230dc5c6edd4f
NotoriousCottonball <gpianka1@gmail.com>
2019-07-13 07:59:54 -0700

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+/*
+(Vs.)
+
+<button onClick={() => props.delete(props.text)}>DELETE</button>
+
+*/
+/*
+Now, when props.delete is called, an action is dispatched that contains an id only as its payload.
+*/
+const Todo = props => (
+  <div>
+      <span>{props.todo.text}</span><button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+  </div>
+)
 
 export default Todo;
