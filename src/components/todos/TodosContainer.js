1faad6ca9934afdff0612e030f4877c067a58ff9
epic0448 <samuelogundiran@Yahoo.com>
2019-11-08 16:36:20 -0500

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Todo from './Todo'
+// import { throws } from 'assert';
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  // renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  /*
+   To be able to use the ID of each created object.  line 7 changes
+   why: The change is minimal, but this set up is actually better. Previously, key was based 
+   off the index provided by map. Now its using our randomly generated ID, 
+   and is less prone to errors in the virtual DOM.
+    We'll need both todo.id and todo.text to be passed into 
+    Todo so we pass both down as the object, todo.
+  */
+  renderTodos = () => {
+       return this.props.todos.map(todo => < Todo delete={this.props.delete} key= {todo.id} todo={todo} />)
+     }
+
 
   render() {
     return(
@@ -15,10 +28,17 @@ class TodosContainer extends Component {
   }
 };
 
-mapStateToProps = state => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText})
+  }
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodosContainer);
